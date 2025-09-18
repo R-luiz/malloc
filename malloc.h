ef5eae023e07f97244e44713d0a035c2f3cea468
r-luiz <lerlfr3@gmail.com>
2025-09-18 09:04:06 +0000

    added .h

---
+++ malloc.h
@@ -0,0 +1,24 @@
+#ifndef MALLOC_H
+# define MALLOC_H
+
+# include <sys/mman.h>
+# include <unistd.h>
+# include <pthread.h>
+# include <stdlib.h>
+
+/* ===== Gestion multi-plateforme ===== */
+# ifdef LINUX
+#  define GET_PAGE_SIZE() sysconf(_SC_PAGESIZE)
+# elif MACOS
+#  define GET_PAGE_SIZE() getpagesize()
+# else
+#  define GET_PAGE_SIZE() 4096  // Fallback par défaut
+# endif
+
+/* ===== Prototypes des fonctions exportées ===== */
+void    free(void *ptr);
+void    *malloc(size_t size);
+void    *realloc(void *ptr, size_t size);
+void    show_alloc_mem(void);
+
+#endif
